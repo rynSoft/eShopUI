39e743b7b92cd578723a0021cdfb1f6d82e00a67
buraksaq <burak.ornek@sentytech.com.tr>
2024-05-24 22:42:55 +0300

    button ekledim

--- src/views/pages/ProductHistoriesExtended/ProductHistoriesExtended.js
+++ src/views/pages/ProductHistoriesExtended/ProductHistoriesExtended.js
@@ -311,6 +311,12 @@ function ProductHistoriesExtended(props) {
                             >
                               <ArrowRightCircle size={16} />
                             </Button>
+                            <Button
+                              className="btn btn-warning"
+                              onClick={() => getHistories(obj.id)}
+                            >
+                              <ArrowRightCircle size={16} />
+                            </Button>
                           </td>
                         </tr>
                       ))}
